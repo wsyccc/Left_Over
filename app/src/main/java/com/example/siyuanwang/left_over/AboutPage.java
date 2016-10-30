258b14f70fb0c5516fd20d8a10a4ab1838ab9047
karl-arreola <karreola@my.bcit.ca>
2016-10-29 18:58:01 -0700

    test

--- app/src/main/java/com/example/siyuanwang/left_over/AboutPage.java
+++ app/src/main/java/com/example/siyuanwang/left_over/AboutPage.java
@@ -8,6 +8,6 @@ public class AboutPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_about_page);
     }
 }
