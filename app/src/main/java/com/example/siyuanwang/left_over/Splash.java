16f597c9c4d9e9b567d7af8d88f23c4f4dbac576
Wayne <wsyccf@gmail.com>
2016-10-29 19:51:00 -0700

    splash init

---
+++ app/src/main/java/com/example/siyuanwang/left_over/Splash.java
@@ -0,0 +1,21 @@
+package com.example.siyuanwang.left_over;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    public void page2 (View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
