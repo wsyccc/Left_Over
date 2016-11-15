e259006dbb0dc7fe81e57c9279cc430166de93ad
Arnold Myint <blinkmonster@gmail.com>
2016-11-14 19:48:47 -0800

    Added a dummy map to NearBy Fragment

--- app/src/main/java/com/bcit/Leftovers/fragment/Nearby_Fragment.java
+++ app/src/main/java/com/bcit/Leftovers/fragment/Nearby_Fragment.java
@@ -66,7 +66,7 @@ public class Nearby_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        return inflater.inflate(R.layout.activity_maps, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
