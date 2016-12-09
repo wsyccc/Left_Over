b49bdb4f2e2c5242767d7f293e818f7994d1a7ef
Wayne <wsyccf@gmail.com>
2016-12-09 15:54:07 -0800

    1111

--- app/src/main/java/com/bcit/Leftovers/other/CommentListAdapter.java
+++ app/src/main/java/com/bcit/Leftovers/other/CommentListAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bcit.Leftovers.R;
